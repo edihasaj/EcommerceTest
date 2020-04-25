75e0e0d089054954989da86654c53d9d49d8b405
drenhaziri <dren.haziri@riinvest.net>
2020-04-25 18:28:04 +0200

    * CategoryTest.cs: comit
    * ProductTest.cs: aaa

--- Tests/CategoryTest.cs
+++ Tests/CategoryTest.cs
@@ -1,10 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
 namespace Tests
 {
+    [TestFixture]
     public class CategoryTest
     {
-        public CategoryTest()
+        Category Category;
+        Category CategoryEmpty;
+        Category CategoryUpdate;
+
+        [SetUp]
+        public void SetUp()
         {
+            Category = new Category()
+            {
+                Id = 1,
+                Name = "Tech",
+                Description = "Technology products",
+                Items = 0
+            };
+
+
+            CategoryEmpty = new Category()
+            {
+                Id = 1,
+                Name = "",
+                Description = "",
+                Items = 0
+            };
+
+            CategoryUpdate = new Category()
+            {
+                Id = 1,
+                Name = "Kitchen",
+                Description = "Nothing",
+                Items = 0
+            };
         }
+
+        [TestCase(1)]
+        public void GetCategoryById_NameTest(int id)
+        {
+            var result = CategoryRepository.GetCategoryById(id);
+
+            Assert.AreEqual(Category.Name, result.Name);
+        }
+
+        [TestCase(1)]
+        public void GetCategoryById_DescriptonTest(int id)
+        {
+            var result = CategoryRepository.GetCategoryById(id);
+
+            Assert.AreEqual(Category.Description, result.Description);
+        }
+
+        [TestCase(1)] // should fail
+        public void GetCategoryById_Test(int id)
+        {
+            Category result = CategoryRepository.GetCategoryById(id);
+
+            Assert.AreEqual(CategoryEmpty, result);
+        }
+
+        [TestCase(1)]
+        public void UpdateCategory_TestById(int id)
+        {
+            CategoryRepository.Update(CategoryUpdate);
+            var resultAfter = CategoryRepository.GetCategoryById(id);
+
+            Assert.AreEqual(CategoryUpdate, resultAfter);
+        }
+
+        [Test]
+        public void CategoryExists_Test()
+        {
+            //var result = CategoryRepository.Exists(Category);
+            //Assert.IsTrue(result);
+        }
+
+
+
     }
 }
--- Tests/ProductTest.cs
+++ Tests/ProductTest.cs
@@ -1,10 +1,63 @@
-﻿using System;
+﻿
+using System.Linq;
+using NUnit.Framework;
+
 namespace Tests
 {
+    [TestFixture]
     public class ProductTest
     {
-        public ProductTest()
+        Product p;
+        Product pEmpty;
+        Product newProduct;
+
+        [SetUp]
+        public void SetUp()
         {
+            p = new Product()
+            {
+                Id = 1,
+                Name = "Macbook Air",
+                Description = "Diqka",
+                PurchasePrice = 899,
+                RetailPrice = 999,
+                CategoryId = 1
+            };
+
+            pEmpty = new Product();
+
+            newProduct = new Product()
+            {
+                Name = "X1 Carbon",
+                Description = "Diqka",
+                PurchasePrice = 1500,
+                RetailPrice = 1850,
+                CategoryId = 1
+            };
+        }
+
+        [Test]
+        public void CountProducts()
+        {
+            int count = ProductRepository.GetProducts().Count;
+
+            Assert.AreEqual(count, 2);
+        }
+
+        [Test]
+        public void CountProducts_ShouldFail()
+        {
+            int count = ProductRepository.GetProducts().Count;
+
+            Assert.AreEqual(count, 3);
+        }
+
+        [Test]
+        public void InsertProduct()
+        {
+            ProductRepository.Insert(newProduct);
+            var result = ProductRepository.GetProducts().Last();
+            Assert.AreEqual(newProduct.Name, result.Name);
         }
     }
 }
